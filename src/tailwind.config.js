48d57231ed48a5b2fba61a4dcf76a77b267199d8
wendyhamel <whamel@it4web.nl>
2024-04-10 13:00:54 +0200

    Startup files added, Mobile and desktop (responsive) styling

---
+++ src/tailwind.config.js
@@ -0,0 +1,20 @@
+tailwind.config = {
+	theme: {
+		fontFamily: {
+			'sans': ['Outfit', 'sans-serif'],
+			'heading': ['Young Serif', 'sans-serif'],
+		},
+		extend: {
+			colors: {
+				'nutmeg': 'hsl(14, 45%, 36%)',
+				'raspberry' : 'hsl(332, 51%, 32%)',
+				'white': 'hsl(0, 0%, 100%)',
+				'rose': 'hsl(330, 100%, 98%)',
+				'eggshell': 'hsl(30, 54%, 90%)',
+				'gray': 'hsl(30, 18%, 87%)',
+				'brown': 'hsl(30, 10%, 34%)',
+				'charcoal': 'hsl(24, 5%, 18%)',
+			},
+		}
+	}
+}
